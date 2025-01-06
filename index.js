f3d3388632e65cc7c48cf03d31384ec1788791c4
Mahbub8340 <mdmahbubalam573125@gmail.com>
2025-01-06 20:27:33 +0600

    update functionallity complete

--- index.js
+++ index.js
@@ -129,13 +129,37 @@ async function run() {
             res.send(result)
         });
 
+        app.get('/menu/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await menuCollections.findOne(query);
+            res.send(result)
+        })
+
         app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
             const menuData = req.body;
             const result = await menuCollections.insertOne(menuData);
             res.send(result)
         });
 
-        app.delete('/menu/:id', async (req, res) => {
+        app.patch('/menu/:id', async(req, res) => {
+            const items = req.body;
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const updateDoc = {
+                $set: {
+                    name: items.name,
+                    category: items.category,
+                    price: items.price,
+                    recipe: items.recipe,
+                    image: items.image
+                }
+            };
+            const result = await menuCollections.updateOne(query, updateDoc);
+            res.send(result)
+        })
+
+        app.delete('/menu/:id',verifyToken, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await menuCollections.deleteOne(query);
